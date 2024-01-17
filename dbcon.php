5294002f7b822e38eb21d00bbd9d87a653dda02b
ghorg2208 <80393622+ghorg2208@users.noreply.github.com>
2024-01-17 15:20:21 +0100

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -8,7 +8,7 @@ if(!$con){
 
 
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL /*"{path to CA cert}"*/, NULL, NULL);
+/*mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);*/
 mysqli_real_connect($conn, "stan-victor.mysql.database.azure.com", "stanvictor", "Azerty1234", "crud", 3306, MYSQLI_CLIENT_SSL);
 
 
